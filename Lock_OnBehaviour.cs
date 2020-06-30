654ac80920893b92495e038529e400b3e3ab5814
PrasinShrestha <65078211+PrasinShrestha@users.noreply.github.com>
2020-06-30 19:30:01 +0545

    Add files via upload

---
+++ Lock_OnBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock_OnBehaviour : MonoBehaviour
+{
+	public GameObject[] Candidates = new GameObject[50];
+	public int candid_num;
+	float[] Candidates_dist = new float[50];
+	float min_dist;
+
+	GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		target = null;
+		min_dist = 1000;
+		candid_num = -1;
+    }
+
+	public GameObject LockTarget()
+	{
+		if (candid_num == -1)
+		{
+			return null;
+		}
+		else
+		{
+			for (int i = 0; i <= candid_num; i++)
+			{
+				Candidates_dist[i] = Vector3.Distance(Candidates[i].transform.position, transform.position);
+				if (Candidates_dist[i] < min_dist)
+				{
+					min_dist = Candidates_dist[i];
+					target = Candidates[i];
+				}
+				Candidates[i] = null;
+			}
+			candid_num = -1;
+			min_dist = 1000;
+			return target;
+		}		
+	}
+}
---
+++ Lock_OnTarget.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock_OnTarget : MonoBehaviour
+{
+	Lock_OnBehaviour lock_onplayer;
+	HitDetection thisobject; 
+	bool potential_Target;
+	bool lock_on;
+
+	void Start()
+	{
+		lock_on = false;
+		potential_Target = false;
+		thisobject = GetComponent<HitDetection>();
+		lock_onplayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Lock_OnBehaviour>();
+	}
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Middle Click"))
+		{
+			if (lock_on == false && potential_Target == true)
+			{
+				lock_onplayer.candid_num += 1;
+				lock_onplayer.Candidates[lock_onplayer.candid_num] = gameObject;
+				lock_on = true;
+			}	
+			else
+			{
+				lock_on = false;
+			}
+			
+		}
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.name == "Player_View")
+		{
+			potential_Target = true;
+		}
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		if (collider.gameObject.name == "Player_View")
+		{
+			potential_Target = false;
+		}
+	}
+}
